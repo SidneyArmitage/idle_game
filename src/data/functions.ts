451665b5862b8ea0aa139617b20cae69119a6293
SidneyArmitage <sichy@live.com.au>
2021-11-02 09:18:59 +1100

    added basic types

---
+++ src/data/functions.ts
@@ -0,0 +1,10 @@
+// storage functions
+// returns false if the operation is illegal
+const tryReserve = (store: IStorage, key: number, value: number): boolean => {
+  throw Error("Not implemented");
+};
+
+// returns false if the operation is illegal
+const tryModify = (key: number, value: number, fillAvailable: boolean): boolean => {
+  throw Error("Not implemented");
+};
